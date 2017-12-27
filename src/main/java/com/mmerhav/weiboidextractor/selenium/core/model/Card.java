7731dfeb1cb3b1fa1f393828d683016cfe7bc5ca
Mor <mormerhav@gmail.com>
2017-12-27 09:02:25 +0200

    Exclude name from equals and hashcode of Card data class

--- src/main/java/com/mmerhav/weiboidextractor/selenium/core/model/Card.java
+++ src/main/java/com/mmerhav/weiboidextractor/selenium/core/model/Card.java
@@ -2,9 +2,11 @@ package com.mmerhav.weiboidextractor.selenium.core.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode(exclude = "name")
 public class Card {
 
     private String id;
